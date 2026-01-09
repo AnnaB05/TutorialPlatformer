a69dc6fdb933e1f6632f4643e13f19d77e8b115e
AnnaB05 <aebake05@louisville.edu>
2026-01-09 16:37:12 -0500

    changed pannel size to 1280x800

--- src/main/java/io/github/AnnaB05/main/Game.java
+++ src/main/java/io/github/AnnaB05/main/Game.java
@@ -4,13 +4,61 @@ package io.github.AnnaB05.main;
  * game class holds everything together
  * this is where everything  will start
  */
-public class Game {
+public class Game implements Runnable {
+
     private GameWindow gameWindow;
     private GamePanel gamePanel;
+    private Thread gameThread;
+    private final int FPS_SET = 120;
+
+    /** constructor for Game **/
     public Game() {
+
         gamePanel = new GamePanel();
         gameWindow = new GameWindow(gamePanel);
         gamePanel.requestFocus();
+
+        startGameLoop();
+
+    }
+
+    //method for gameThread
+    private void startGameLoop() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    /**
+     * this is the where the game loop will be
+     */
+    @Override
+    public void run() {
+
+        double timePerFrame = 1000000000.0 / FPS_SET; //each frame should last 1 million nanoseconds
+        long lastFrame = System.nanoTime();
+        long now = System.nanoTime();
+        int frames = 0;
+        long lastCheck = System.currentTimeMillis();
+        while(true) {
+
+            now = System.nanoTime();
+            if(now - lastFrame >= timePerFrame) {
+
+                gamePanel.repaint();
+                lastFrame = now;
+                frames++;
+            }
+
+
+            if(System.currentTimeMillis() - lastCheck >= 1000) {
+                lastCheck = System.currentTimeMillis();
+                System.out.println("FPS: " + frames);  //prints frames per second to console
+                frames = 0; //resets frame count
+            }
+
+
+
+        }
 
     }
 }
--- src/main/java/io/github/AnnaB05/main/GamePanel.java
+++ src/main/java/io/github/AnnaB05/main/GamePanel.java
@@ -8,23 +8,38 @@ import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Random;
 
 /**
  * panel for game graphics to be drawn
  */
 public class GamePanel extends JPanel {
+
     private MouseInputs mouseInputs;
-    private float xDelta = 100, yDelta = 100;
-    private float xDir = 0.01f, yDir = 0.01f;
+    private float xDelta = 100, yDelta = 100; //sets rect's initial position
+    private float xDir = 1f, yDir = 1f; //controls rect movement speed
     private int frames = 0;
     private long lastCheck = 0;
+    private Color color = new Color(150,20,90); //initial rectangle color
+    private Random random;
 
     public GamePanel() {
+        random = new Random();
         mouseInputs = new MouseInputs(this);
         addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
         addMouseMotionListener(mouseInputs);
 
+        setPanelSize(); // sets panel size
+        setFocusable(true); // allows panel to receive focus and key inputs
+
+    }
+    // sets panel size
+    private void setPanelSize() {
+        Dimension size = new Dimension(1280,800);
+        setMinimumSize(size);
+        setPreferredSize(size);
+        setMaximumSize(size);
     }
 
     public void changeXDelta(int value) {
@@ -49,26 +64,35 @@ public class GamePanel extends JPanel {
         super.paintComponent(g);
 
         updateRectangle();
+        g.setColor(color);
 
         g.fillRect((int) xDelta, (int) yDelta, 200, 50);
+    }
 
-        frames++;
-        if(System.currentTimeMillis() - lastCheck >= 1000) {
-            lastCheck = System.currentTimeMillis();
-            System.out.println("FPS: " + frames);
-            frames = 0;
-        }
+    private void updateRectangle() {
+       xDelta += xDir;
+       if (xDelta > 400 || xDelta < 0) {
+           xDir *= -1;
+           color = getRndColor();
+       }
 
-        repaint();
+       yDelta += yDir;
+       if (yDelta > 400 || yDelta < 0) {
+           yDir *= -1;
+           color = getRndColor();
+       }
+
 
     }
 
-    public void updateRectangle() {
-       xDelta += xDir;
-       if (xDelta > 400 || xDelta < 0)
-           xDir = -1;
-       yDelta += yDir;
-       if (yDelta > 400 || yDelta < 0)
-           yDir = -1;
+    /**
+     * generates random color
+     */
+    private Color getRndColor() {
+        int r = random.nextInt(255);
+        int g = random.nextInt(255);
+        int b = random.nextInt(255);
+
+        return new Color(r,g,b);
     }
 }
