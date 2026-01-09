6f25c6adbd8b45af07d2c2e218b373737bea196a
AnnaB05 <aebake05@louisville.edu>
2026-01-09 11:48:53 -0500

    rectangle now move on its own

--- src/main/java/io/github/AnnaB05/main/GamePanel.java
+++ src/main/java/io/github/AnnaB05/main/GamePanel.java
@@ -14,8 +14,10 @@ import java.awt.event.KeyListener;
  */
 public class GamePanel extends JPanel {
     private MouseInputs mouseInputs;
-    private int xDelta = 100;
-    private int yDelta = 100;
+    private float xDelta = 100, yDelta = 100;
+    private float xDir = 0.01f, yDir = 0.01f;
+    private int frames = 0;
+    private long lastCheck = 0;
 
     public GamePanel() {
         mouseInputs = new MouseInputs(this);
@@ -27,17 +29,17 @@ public class GamePanel extends JPanel {
 
     public void changeXDelta(int value) {
         this.xDelta += value;
-        repaint();
+
     }
     public void changeYDelta(int value) {
         this.yDelta += value;
-        repaint();
+
     }
 
     public void setRectPosition(int x, int y) {
         this.xDelta = x;
         this.yDelta = y;
-        repaint();
+
     }
 
     /**
@@ -46,7 +48,27 @@ public class GamePanel extends JPanel {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        g.fillRect(xDelta, yDelta, 200, 50);
+        updateRectangle();
 
+        g.fillRect((int) xDelta, (int) yDelta, 200, 50);
+
+        frames++;
+        if(System.currentTimeMillis() - lastCheck >= 1000) {
+            lastCheck = System.currentTimeMillis();
+            System.out.println("FPS: " + frames);
+            frames = 0;
+        }
+
+        repaint();
+
+    }
+
+    public void updateRectangle() {
+       xDelta += xDir;
+       if (xDelta > 400 || xDelta < 0)
+           xDir = -1;
+       yDelta += yDir;
+       if (yDelta > 400 || yDelta < 0)
+           yDir = -1;
     }
 }
