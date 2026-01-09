65985b7c62b9952bf16b4bf131e8ba5085d55a7a
AnnaB05 <aebake05@louisville.edu>
2026-01-09 11:02:35 -0500

    adding lost code back in

--- src/main/java/io/github/AnnaB05/main/GamePanel.java
+++ src/main/java/io/github/AnnaB05/main/GamePanel.java
@@ -14,13 +14,22 @@ import java.awt.event.KeyListener;
  */
 public class GamePanel extends JPanel {
     private MouseInputs mouseInputs;
+    private int xDelta = 0;
+    private int yDelta = 0;
 
     public GamePanel() {
         mouseInputs = new MouseInputs();
-        addKeyListener(new KeyboardInputs());
+        addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
         addMouseMotionListener(mouseInputs);
 
+    }
+
+    public void changeXDelta(int value) {
+        this.xDelta += value;
+    }
+    public void changeYDelta(int value) {
+        this.yDelta += value;
     }
 
     /**
@@ -29,7 +38,7 @@ public class GamePanel extends JPanel {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        g.fillRect(100, 100, 200, 50);
+        g.fillRect(100 + xDelta, 100 + yDelta, 200, 50);
 
     }
 }
