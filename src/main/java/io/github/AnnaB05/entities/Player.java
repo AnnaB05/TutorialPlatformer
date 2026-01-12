1073a92009ef1231f0254ce51a4a332d51107b4b
AnnaB05 <aebake05@louisville.edu>
2026-01-11 20:42:28 -0500

    simplified try catch in gamePanel

--- src/main/java/io/github/AnnaB05/entities/Player.java
+++ src/main/java/io/github/AnnaB05/entities/Player.java
@@ -1,6 +1,12 @@
 package io.github.AnnaB05.entities;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class Player extends Entity {
+    private BufferedImage[][] animations; // array to hold idle animation frames
     public Player(float x, float y) {
         super(x, y);
     }
@@ -11,5 +17,27 @@ public class Player extends Entity {
 
     public void render() {
         // Render player on screen
+    }
+
+    /// method for handling the player character's animations
+    private void loadAnimations() {
+
+        InputStream is = getClass().getResourceAsStream("/YellowGuy.png");
+
+
+        /*try (InputStream is = getClass().getResourceAsStream("/YellowGuy.png")) {
+
+            if (is == null) {
+                throw new RuntimeException("Resource `/YellowGuy.png` not found");
+            }
+            img = ImageIO.read(is);
+            if (img == null) {
+                throw new RuntimeException("Failed to read `/YellowGuy.png`");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }*/
+
+
     }
 }
--- src/main/java/io/github/AnnaB05/main/GamePanel.java
+++ src/main/java/io/github/AnnaB05/main/GamePanel.java
@@ -53,7 +53,6 @@ public class GamePanel extends JPanel {
     /// method for handling the player character's animations
     private void loadAnimations() {
         animations = new BufferedImage[7][12]; // 7 animations, with the highest frame count being 12
-
         for(int j = 0; j < animations.length; j++) {
             for(int i =0; i < animations[j].length; i++) {
                 animations[j][i] = img.getSubimage(i*32, j*32, 32, 32);
@@ -64,18 +63,20 @@ public class GamePanel extends JPanel {
     /// method for handling img imports, try catch in case img cannot be found
     private void importImg() {
 
-        try (InputStream is = getClass().getResourceAsStream("/YellowGuy.png")) {
-            if (is == null) {
-                throw new RuntimeException("Resource `/YellowGuy.png` not found");
-            }
+        InputStream is = getClass().getResourceAsStream("/YellowGuy.png");
+        try {
             img = ImageIO.read(is);
-            if (img == null) {
-                throw new RuntimeException("Failed to read `/YellowGuy.png`");
-            }
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
+        } finally {
+            try{
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-    }
+
+
 
     // sets panel size
     private void setPanelSize() {
