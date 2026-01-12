60d6f03c1584aa8652011f765b31ffbc1bfc3e08
AnnaB05 <aebake05@louisville.edu>
2026-01-11 20:28:07 -0500

    created new package entities too hold enemies aand other characters

---
+++ src/main/java/io/github/AnnaB05/entities/Entity.java
@@ -0,0 +1,9 @@
+package io.github.AnnaB05.entities;
+
+public abstract class Entity {
+    protected float x, y;
+    public Entity (float x, float y){
+        this.x = x;
+        this.y = y;
+    }
+}
