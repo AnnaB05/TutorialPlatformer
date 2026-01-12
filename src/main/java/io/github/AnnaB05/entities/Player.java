8e001b160c65793a5801b788f1543df06576b4d2
AnnaB05 <aebake05@louisville.edu>
2026-01-11 20:31:09 -0500

    new Player class extends Entity

---
+++ src/main/java/io/github/AnnaB05/entities/Player.java
@@ -0,0 +1,15 @@
+package io.github.AnnaB05.entities;
+
+public class Player extends Entity {
+    public Player(float x, float y) {
+        super(x, y);
+    }
+
+    public void update() {
+        // Update player state
+    }
+
+    public void render() {
+        // Render player on screen
+    }
+}
