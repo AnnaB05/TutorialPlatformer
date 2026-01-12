77b606d290fd9a1a508f44628d627ee61e1ad2f5
AnnaB05 <aebake05@louisville.edu>
2026-01-12 11:05:31 -0500

    added getters and setters for left/right/up/down in Player class

--- src/main/java/io/github/AnnaB05/entities/Player.java
+++ src/main/java/io/github/AnnaB05/entities/Player.java
@@ -84,8 +84,39 @@ public class Player extends Entity {
             }
         }
 
+
+
     }
 
 
+    public boolean isLeft() {
+        return left;
+    }
 
+    public void setLeft(boolean left) {
+        this.left = left;
+    }
+
+    public boolean isRight() {
+        return right;
+    }
+
+    public void setRight(boolean right) {
+        this.right = right;
+    }
+
+    public boolean isUp() {
+        return up;
+    }
+
+    public void setUp(boolean up) {
+        this.up = up;
+    }
+
+    public boolean isDown() {
+        return down;
+    }
+    public void setDown(boolean down) {
+        this.down = down;
+    }
 }
