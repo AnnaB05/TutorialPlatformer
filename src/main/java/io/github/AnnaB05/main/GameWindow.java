100917770939ff9376d09d4acc6c20077fa1cbb3
AnnaB05 <aebake05@louisville.edu>
2026-01-14 13:53:39 -0500

    added new event listener to GameWindow that should prevent character from running off the screen whenever you click outside of the game panel. Runing into issue where the game panel dissapears when you click outside of it

--- src/main/java/io/github/AnnaB05/main/GameWindow.java
+++ src/main/java/io/github/AnnaB05/main/GameWindow.java
@@ -1,6 +1,8 @@
 package io.github.AnnaB05.main;
 
 import javax.swing.*;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowFocusListener;
 
 /**
  * class for game window
@@ -17,8 +19,20 @@ public class GameWindow {
         jframe.add(gamePanel); // Add the game panel to the window
         jframe.setLocationRelativeTo(null); // Center the window on the screen
         jframe.setResizable(false); // Prevent resizing of the window
-        jframe.pack();
+        jframe.pack(); // Adjust the window size to fit the preferred size of its components
         jframe.setVisible(true); // Make the window visible
+        jframe.addWindowFocusListener(new WindowFocusListener() {
+            @Override
+            public void windowGainedFocus(WindowEvent e) {
+
+
+            }
+
+            @Override
+            public void windowLostFocus(WindowEvent e) {
+                System.out.println("BYEE");
+            }
+        });
 
     }
 }
