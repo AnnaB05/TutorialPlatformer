99eea2356d11bfc277efdcf27a9198dae6b53c04
AnnaB05 <aebake05@louisville.edu>
2026-01-16 11:23:12 -0500

    copied over loadAnimations code from Player class to static method in loadSave class. removed the animations chunk and moved BufferedImage ing to the top of the method so that img is always initialized

--- src/main/java/io/github/AnnaB05/utilz/loadSave.java
+++ src/main/java/io/github/AnnaB05/utilz/loadSave.java
@@ -1,4 +1,26 @@
 package io.github.AnnaB05.utilz;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class loadSave {
+    public static BufferedImage GetPlayerAtlas() {
+        BufferedImage img = null;
+        InputStream is = loadSave.class.getResourceAsStream("/YellowGuy.png");
+        try {
+            img = ImageIO.read(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return img;
+
+    }
 }
